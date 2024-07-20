bffda28c663b542dfd7aaabbce44fb9c030b877d
matheusz-nied <matheustae@hotmail.com>
2024-07-20 12:53:24 -0300

    feat: sorting data

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ],
+  ],  define: {
+    'process.env': {},
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
